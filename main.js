0afee8d84a66a4e303acd2fd4f597970c6317a00
uniquepoolmanagement <joshuawilkeson@timetracker365.com>
2019-07-11 10:52:01 -0500

    collapsible function is not needed

--- main.js
+++ main.js
@@ -94,21 +94,3 @@
     return this;
   };
 })(jQuery, window, document);
-
-function collapsible() {
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      }
-    });
-  }
-}
